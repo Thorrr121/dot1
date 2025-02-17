efde892fa63973b751423950b1dbd06fe9be4365
Thorrr121 <bgmidata0013@gmail.com>
2025-02-17 17:50:13 +0530

    Update m.py

--- m.py
+++ m.py
@@ -7,7 +7,7 @@ import datetime
 import os
 
 # insert your Telegram bot token here
-bot = telebot.TeleBot('7492176838:AAGOSrpD131zwoQRphflWFk7IR1kuVLJGs')
+bot = telebot.TeleBot('7231434237:AAFPx4oJi_mhYQZuwlIBJ47px2CoRBRXWXI')
 
 # Admin user IDs
 admin_id = ["1383324178","948895728"]
